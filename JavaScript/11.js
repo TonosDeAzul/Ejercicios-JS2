f5864e5a3eb0de791399ea25567ede6bda83e176
TonosDeAzul <dg.wrq109@gmail.com>
2024-03-15 01:01:49 -0500

    avance 2

---
+++ JavaScript/11.js
@@ -0,0 +1,22 @@
+/*
+Obtén una serie de número y devuelve la suma de todos los números positivos, si no tenemos números positivos la sume debe devolver 0.
+*/
+
+let nums = [1, 2, 3, -4, 5, 6, -7];
+
+function sumaNumsPositivos(serie){
+
+    let sum = 0;
+    for(let i = 0; i < serie.length; i++){
+        if(serie[i] < 0){
+            continue;
+        }
+        else{
+            sum += serie[i];
+        }
+    }
+    console.log(`La suma de los números es: ${sum}`);
+
+}
+
+sumaNumsPositivos(nums);
---
+++ JavaScript/12.js
@@ -0,0 +1,4 @@
+/*
+Implemente una función que sume dos números y devuelva su suma en binario, el número binario devuelto debe ser una cadena.
+*/
+
